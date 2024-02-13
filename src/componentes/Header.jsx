00e85458faef466947cdf61f26c44cfe879913df
jmcord <jcorderodz@hotmail.com>
2024-02-13 19:57:13 +0100

    check

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,10 +1,14 @@
+import { ConnectKitButton } from 'connectkit'
+
 export default function Header() {
-    return (
-      <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs">
-        {/* Logo para version mobile */}
-        <img src="/bm.png" alt="blockmaker-logo" width={47} className="sm:hidden" />
-        {/* Logo para version desktop */}
-        <img src="/logo-blockmaker.png" alt="blockmaker-logo" width={300} className="hidden sm:flex" />
-      </header>
-    )
-  }
+  return (
+    <header className="py-2 px-3 sm:py-4 sm:px-8 flex justify-between items-center bg-white border-b shadow-xs">
+      {/* Logo para version mobile */}
+      <img src="/bm.png" alt="blockmaker-logo" width={47} className="sm:hidden" />
+      {/* Logo para version desktop */}
+      <img src="/logo-blockmaker.png" alt="blockmaker-logo" width={300} className="hidden sm:flex" />
+      {/* Botton de conexion wallet con prop showBalance para mostrar balance cuenta */}
+      <ConnectKitButton showBalance />
+    </header>
+  )
+}
