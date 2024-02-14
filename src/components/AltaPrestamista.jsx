e9e29fb93d3d3699571c92d304965b028af917d2
jmcord <jcorderodz@hotmail.com>
2024-02-14 12:13:54 +0100

    c

--- src/components/AltaPrestamista.jsx
+++ src/components/AltaPrestamista.jsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from 'react'
-import { toast } from 'react-hot-toast'
-import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi'
-import { blockmakerTokenABI } from '../contracts/ABIs'
-import { Button, TextInput, Title } from './ui'
+import { Button, TextInput, Title } from './ui';
+import { useState, useEffect } from 'react';
+import { useContractWrite, usePrepareContractWrite, useWaitForTransaction } from 'wagmi';
+import { blockmakerTokenABI } from '../contracts/ABIs';
 
-export default function AltaPrestamista() {
-  const [to, setTo] = useState('')
-  const [amount, setAmount] = useState('')
+export default function MintTokensForm() {
+  const [to, setTo] = useState('');
+  const [amount, setAmount] = useState('');
 
   const { config } = usePrepareContractWrite({
     address: import.meta.env.VITE_TOKEN_CONTRACT_ADDRESS,
     abi: blockmakerTokenABI,
-    functionName: 'transfer',
-    enabled: to && amount > 0,
-    args: [to, BigInt(amount * 10 ** 18)]
-  })
+    functionName: 'altaPrestamista',
+    args: [to]
+  });
 
-  const { data: writeData, write } = useContractWrite(config)
+  const { data: writeData, write } = useContractWrite(config);
 
   const {
     isLoading: isTransactionLoading,
@@ -24,55 +22,38 @@ export default function AltaPrestamista() {
     isError: isTransactionError
   } = useWaitForTransaction({
     hash: writeData?.hash
-  })
+  });
 
-  const handleToInputChange = (e) => {
-    setTo(e.target.value)
-  }
+  const handlerToInputChange = (event) => {
+    setTo(event.target.value);
+  };
 
-  const handleAmountInputChange = (e) => {
-    setAmount(e.target.value)
-  }
+  const handlerAmountInputChange = (event) => {
+    setAmount(event.target.value);
+  };
 
   useEffect(() => {
     if (isTransactionSuccess) {
-      toast.success('Se han transferido los tokens con éxito.')
-      setTo('')
-      setAmount('')
+      setTo('');
+      setAmount('');
+      console.log('Transacción Completada!');
     }
     if (isTransactionError) {
-      toast.error('No se ha podido realizar la transacción. Prueba de nuevo más tarde.')
+      console.log('Transacción Fallida!');
     }
-  }, [isTransactionSuccess, isTransactionError])
+  }, [isTransactionSuccess, isTransactionError]);
 
   return (
-    <section className="p-4 bg-white border shadow rounded-lg text-sm w-[360px] sm:w-[469px]">
-      <div className="flex gap-1">
-        <Title>Transfer</Title>
-      </div>
+    <section className="bg-white p-4 border shadow rounded-md">
+      <Title>MintForm</Title>
+
       <form className="grid gap-4">
-        <TextInput
-          type="text"
-          placeholder="To"
-          value={to}
-          disabled={isTransactionLoading}
-          onChange={handleToInputChange}
-        />
-        <TextInput
-          type="number"
-          placeholder="Amount"
-          value={amount}
-          disabled={isTransactionLoading}
-          onChange={handleAmountInputChange}
-        />
-        <Button
-          disabled={!to || !amount || isTransactionLoading}
-          isLoading={isTransactionLoading}
-          onClick={() => write?.()}
-        >
-          {isTransactionLoading ? 'Transfiriendo BM Tokens...' : 'Transferir BM Tokens'}
+        <TextInput type="text" placeholder="address" value={to} onChange={handlerToInputChange} />
+
+        <Button disabled={!write || isTransactionLoading} onClick={() => write?.()} isLoading={isTransactionLoading}>
+          {isTransactionLoading ? 'Dando de alta...' : 'Dar de alta Prestamista'}
         </Button>
       </form>
     </section>
-  )
-}
+  );
+} 
