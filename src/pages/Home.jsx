305ee95f7b4826b0bc5eeb72d8412e764c4d2ae8
jmcord <jcorderodz@hotmail.com>
2024-02-15 11:04:01 +0100

    home flex first

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,23 @@
-import { AprobarPrestamo, ObtenerPrestamosPorPrestatario, Owner, SolicitarPrestamo } from '../components'
-import { AltaPrestamista, AltaCliente, DepositarGarantia } from '../components'
-import ReembolsarPrestamo from '../components/ReembolsarPrestamo'
+import React from 'react';
+import { AprobarPrestamo, ObtenerPrestamosPorPrestatario, Owner, SolicitarPrestamo } from '../components';
+import { AltaPrestamista, AltaCliente, DepositarGarantia } from '../components';
+import ReembolsarPrestamo from '../components/ReembolsarPrestamo';
 
 export default function Home() {
   return (
-    <section className="flex flex-col place-items-center gap-4 py-20 px-20">
+    <section className="flex flex-col items-center justify-center gap-4 py-20 px-20">
       <h1>Prestamos DeFi</h1>
 
-      <Owner />
-      <AltaPrestamista />
-      <AltaCliente />
-      <DepositarGarantia />
-      <SolicitarPrestamo />
-      <AprobarPrestamo />
-      <ReembolsarPrestamo />
-      <ObtenerPrestamosPorPrestatario />
-      
- 
+      <div className="flex flex-wrap justify-center gap-4">
+        <Owner />
+        <AltaPrestamista />
+        <AltaCliente />
+        <DepositarGarantia />
+        <SolicitarPrestamo />
+        <AprobarPrestamo />
+        <ReembolsarPrestamo />
+        <ObtenerPrestamosPorPrestatario />
+      </div>
     </section>
-  )
+  );
 }
