f6ab3859e4b5abb2f75e66598cabd6ca5c5848a9
jmcord <jcorderodz@hotmail.com>
2024-02-15 11:44:01 +0100

    c

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export default function Footer() {
-    return (
-      <footer className="py-4 px-3 sm:p-8 flex justify-center bg-white border-t shadow-xs">
-        <p>Derechos de autor &copy; {new Date().getFullYear()} Blockmaker Academy</p>
-      </footer>
-    )
-  }
+  return (
+    <footer className="py-4 px-3 sm:p-8 flex justify-center bg-white border-t shadow-xs" style={{ backgroundImage: 'url(/public/bc2.png)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+      <p style={{ color: 'white' }}>Derechos de autor &copy; {new Date().getFullYear()} Blockmaker Academy</p>
+    </footer>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,22 @@ import ReembolsarPrestamo from '../components/ReembolsarPrestamo';
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-20 px-20" style={{ backgroundImage: 'url(/public/bc2.png)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
-      <h1>Prestamos DeFi</h1>
-      <div className="flex flex-wrap justify-center gap-4">
-        <Owner />
-        <AltaPrestamista />
-        <AltaCliente />
-        <DepositarGarantia />
-        <SolicitarPrestamo />
-        <AprobarPrestamo />
-        <ReembolsarPrestamo />
-        <ObtenerPrestamosPorPrestatario />
-      </div>
+    <section className="flex flex-col items-center justify-center gap-4 py-20 px-20">
+      <section className="flex flex-col place-items-center gap-4 py-20 px-20" style={{ backgroundImage: '/public/bc.jpg")', backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}></section>
+        <h1>Prestamos DeFi</h1>
+
+        <div className="flex flex-wrap justify-center gap-4">
+          <Owner />
+          <AltaPrestamista />
+          <AltaCliente />
+          <DepositarGarantia />
+          <SolicitarPrestamo />
+          <AprobarPrestamo />
+          <ReembolsarPrestamo />
+          <ObtenerPrestamosPorPrestatario />
+        </div>
+
     </section>
   );
 }
-
-
 
