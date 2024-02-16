eec96d2095042daeb21a414851f63550e64722f9
jmcord <jcorderodz@hotmail.com>
2024-02-16 17:13:09 +0100

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,34 @@
-// Prestamistas.jsx
 import React from 'react';
-import { AltaPrestamista } from '../components';
 
-export default function Prestamistas() {
+function About() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-20 px-20">
-      <h1>Gestión de Prestamistas</h1>
-
-      <div className="flex flex-wrap justify-center gap-4">
-        <AltaPrestamista />
-        {/* Otros componentes relacionados con la gestión de prestamistas */}
+    <div className="container mx-auto mt-8">
+      <h1 className="text-2xl font-bold mb-4">Sobre mí</h1>
+      <p className="mb-4">
+        ¡Hola! Soy [Tu Nombre], un apasionado desarrollador de software con experiencia en [Áreas de experiencia]. Me encanta aprender y trabajar en proyectos desafiantes que me permitan crecer profesionalmente.
+      </p>
+      <p className="mb-4">
+        Puedes encontrarme en LinkedIn y GitHub:
+      </p>
+      <div className="flex">
+        <a
+          href="[Tu LinkedIn]"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mr-4"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="[Tu GitHub]"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
       </div>
-    </section>
+    </div>
   );
 }
+
+export default About;
